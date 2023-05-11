06e5b5da98ea7b83f12cbb302f46be9b0d3d2553
Jose Chavez <2410bautista@gmail.com>
2023-05-11 05:28:50 -0500

    presupuesto

---
+++ backend/src/presupuesto/presupuesto.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { CreatePresupuestoDTO } from './dto/presupuesto.dto';
+import { Presupuesto } from './interfaces/presupuesto.interface';
+
+@Injectable()
+export class PresupuestoService {
+
+    constructor(@InjectModel('Presupuesto') readonly presupuestoModel: Model<Presupuesto>){
+    }
+
+    async getPresupuestos(): Promise<Presupuesto[]>{
+        const presupuestos=await this.presupuestoModel.find();
+        return presupuestos;
+    }
+
+    async getPresupuesto(presupuestoID: string): Promise<Presupuesto>{
+        const getPresupuesto= await this.presupuestoModel.findById(presupuestoID);
+        return getPresupuesto;
+    }
+
+    async createPresupuesto(createPresupuestoDTO:CreatePresupuestoDTO): Promise<Presupuesto>{
+        const presupuesto = new this.presupuestoModel(createPresupuestoDTO);
+        return await presupuesto.save();
+    }
+
+    async updatePresupuesto(presupuestoID: string,createPresupuestoDTO:CreatePresupuestoDTO):Promise<Presupuesto>{
+        const presupuesto = await this.presupuestoModel.findByIdAndUpdate(presupuestoID,createPresupuestoDTO,{new:true})
+        return presupuesto
+    }
+
+    async deletePresupuesto(presupuestoID: string):Promise<Presupuesto>{
+        const deletePresupuesto= await this.presupuestoModel.findByIdAndDelete(presupuestoID);
+        return deletePresupuesto;
+    }
+
+}
