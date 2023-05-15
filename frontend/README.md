d042d0f6967c7460972fa156f1da17fe745605d8
Dixon Briones <dixonbriones@hotmail.com>
2023-05-14 23:58:09 -0500

    arreglos

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# Bomberospanel
+# Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
 
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  rutaAuth: "http://127.0.0.1:3000/auth",
+  rutaAuth: "http://165.232.156.248:30300/auth",
 
 };
